82fead92fe9b099d742b1bfd7a5a3f653afd569b
vaibhav <vaibhav.jani@byjus.com>
2016-04-15 14:08:30 +0530

    Blinking with random color

--- app/src/main/java/com/example/vaibhav/uifactory/activity/MatchActivity.java
+++ app/src/main/java/com/example/vaibhav/uifactory/activity/MatchActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.vaibhav.uifactory.R;
+import com.example.vaibhav.uifactory.utils.PixelUtils;
 
 public class MatchActivity extends AppCompatActivity {
 
@@ -80,7 +81,7 @@ public class MatchActivity extends AppCompatActivity {
                         if (tvStart.getVisibility() == View.VISIBLE) {
                             tvStart.setVisibility(View.INVISIBLE);
                         } else {
-                            //tvStart.setTextColor(PixelUtils.getRandomColor());
+                            tvStart.setTextColor(PixelUtils.getRandomColor());
                             tvStart.setVisibility(View.VISIBLE);
                         }
                         blink();
